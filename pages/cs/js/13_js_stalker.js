f96a579ac2e38ed722eda354e099abd5c4c80a15
Bernhard Mayrhofer <35584158+BernhardMayrhofer@users.noreply.github.com>
2021-09-05 18:40:47 +0200

    Create 13_js_stalker.js

---
+++ pages/cs/js/13_js_stalker.js
@@ -0,0 +1,7 @@
+var name1 = prompt("What is your first name ?");
+var name2 = prompt("What is your second name ?");
+var age = prompt("How old are you?");
+alert("Your name is "+name1+" "+name2);
+alert("You are "+age+" years old");
+alert("These translate in "+(age*365)+" days");
+console.log("Hi "+name1+" "+name2+" !");
