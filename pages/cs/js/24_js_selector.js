e542616b7b574b93cbf3163aa9a0a51932973d59
Bernhard Mayrhofer <35584158+BernhardMayrhofer@users.noreply.github.com>
2021-09-07 03:08:56 +0200

    Create 24_js_selector.js

---
+++ pages/cs/js/24_js_selector.js
@@ -0,0 +1,8 @@
+var p1 = document.getElementById("first");
+console.log(p1);
+var p2 = document.getElementsByClassName("special")[0];
+console.log(p2);
+var p3 = document.querySelector("p");
+console.log(p3);
+var p4 = document.querySelectorAll("p")[0];
+console.log(p4);
